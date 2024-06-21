19b76b1e345ab497bb964f5cbd75d16fa1b857c3
Vishakha Mangtani <135515890+vishakhamangtani23@users.noreply.github.com>
2024-06-21 16:59:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,252 @@
+let temp = {
+  _id: "63e4c84f68aaae5c5d73e293",
+  creator: {
+    user_id: "6527bd2297205a7cd2870a43",
+    first_name: "Eighty ",
+    last_name: "Days",
+    username: "eightydays",
+    email: "eightydays@gmail.com",
+    profile_photo:
+      "https://myphatmaps.com:3000/profile_images/default_image.jpeg",
+  },
+  collaborators: [],
+  title: "Bir, Himachal",
+  long_description: "",
+  short_description: "",
+  created_date: "2023-02-09T10:17:51.000Z",
+  updated_date: "2023-02-09T10:17:51.000Z",
+  is_paid: 0,
+  publish_status: 1,
+  weblink: "",
+  cover_photo: "google_places/images/ChIJyYBJiCW5BDkR2x3L7uFhauU/01",
+  stats: {
+    view_count: 88,
+    place_count: 4,
+    saved_count: 1,
+    comment_count: 0,
+    share_count: {
+      dm_share_count: 0,
+      external_share_count: 0,
+      copy_link_count: 0,
+    },
+    purchased_count: 0,
+  },
+  media: [
+    {
+      uploaded_by: "61ae26368d9a393258e89bcd",
+      media_path: "google_places/images/ChIJyYBJiCW5BDkR2x3L7uFhauU/01",
+      is_google: 1,
+      uploaded_type: 2,
+      thumbnail: "",
+      is_video: false,
+      place: {
+        place_id: "63e4c84f68aaae5c5d73e295",
+        name: null,
+      },
+    },
+    {
+      uploaded_by: "61ae26368d9a393258e89bcd",
+      media_path: "google_places/images/ChIJrdU7WVW5BDkROvi6WcKeuS0/01",
+      is_google: 1,
+      uploaded_type: 2,
+      thumbnail: "",
+      is_video: false,
+      place: {
+        place_id: "63e4c84f68aaae5c5d73e296",
+        name: null,
+      },
+    },
+    {
+      uploaded_by: {
+        $oid: "61ae26368d9a393258e89bcd",
+      },
+      media_path: "google_places/images/ChIJjWeQbbm4BDkRrh_HXb9NNTk/01",
+      is_google: 1,
+      uploaded_type: 2,
+      thumbnail: "",
+      is_video: false,
+      place: {
+        place_id: "63bd2bcdaf376055b18ad664",
+        name: null,
+      },
+    },
+    {
+      uploaded_by: "61ae26368d9a393258e89bcd",
+      media_path: "google_places/images/ChIJzUn2D7q4BDkRGnj0aJ3E_fM/01",
+      is_google: 1,
+      uploaded_type: 2,
+      thumbnail: "",
+      is_video: false,
+      place: {
+        place_id: "63e4c84f68aaae5c5d73e298",
+        name: null,
+      },
+    },
+  ],
+  current_price: 0,
+  prices: [],
+  countries: [
+    {
+      name: "India",
+      country_code: "IN",
+      google_place_id: "ChIJkbeSa_BfYzARphNChaFPjNc",
+      coordinates: {
+        latitude: "20.5937",
+        longitude: "78.9629",
+      },
+    },
+  ],
+  viewed_by: [
+    "61ae26368d9a393258e89bcd",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+    "62c6a621d550ce70ef5dd4aa",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+    "61ae6416f04c553fde8fabe2",
+  ],
+  saved_by: ["61ae6416f04c553fde8fabe2"],
+  places: [
+    {
+      place_id: "63e4c84f68aaae5c5d73e295",
+      google_place_id: "ChIJyYBJiCW5BDkR2x3L7uFhauU",
+      name: "The Bunker Bir",
+      categories: [
+        {
+          is_primary: 1,
+          category_id: 7,
+          name: "Stay",
+        },
+      ],
+      description: "",
+      is_custom: 0,
+      rating: 4.8,
+      review_count: 546,
+      country: {
+        name: "India",
+        country_code: "IN",
+        google_place_id: "ChIJkbeSa_BfYzARphNChaFPjNc",
+      },
+      coordinates: {
+        latitude: "76.726739",
+        longitude: "32.0384284",
+      },
+    },
+    {
+      place_id: "63e4c84f68aaae5c5d73e296",
+      google_place_id: "ChIJrdU7WVW5BDkROvi6WcKeuS0",
+      name: "Amma di rasaoi",
+      categories: [
+        {
+          is_primary: 1,
+          category_id: 1,
+          name: "Food",
+        },
+      ],
+      description: "",
+      is_custom: 0,
+      rating: 4.7,
+      review_count: 24,
+      country: {
+        name: "India",
+        country_code: "IN",
+        google_place_id: "ChIJkbeSa_BfYzARphNChaFPjNc",
+      },
+      coordinates: {
+        latitude: "76.7242873",
+        longitude: "32.0355786",
+      },
+    },
+    {
+      place_id: "63bd2bcdaf376055b18ad664",
+      google_place_id: "ChIJjWeQbbm4BDkRrh_HXb9NNTk",
+      name: "Garden Cafe",
+      categories: [
+        {
+          is_primary: 1,
+          category_id: 1,
+          name: "Food",
+        },
+      ],
+      description: "",
+      is_custom: 0,
+      rating: 4.4,
+      review_count: 1045,
+      country: {
+        name: "India",
+        country_code: "IN",
+        google_place_id: "ChIJkbeSa_BfYzARphNChaFPjNc",
+      },
+      coordinates: {
+        latitude: "32.0339098",
+        longitude: "76.7182521",
+      },
+    },
+    {
+      place_id: "63e4c84f68aaae5c5d73e298",
+      google_place_id: "ChIJzUn2D7q4BDkRGnj0aJ3E_fM",
+      name: "Cafe Cloud Door",
+      categories: [
+        {
+          is_primary: 1,
+          category_id: 7,
+          name: "Stay",
+        },
+      ],
+      description: "",
+      is_custom: 0,
+      rating: 4.6,
+      review_count: 178,
+      country: {
+        name: "India",
+        country_code: "IN",
+        google_place_id: "ChIJkbeSa_BfYzARphNChaFPjNc",
+      },
+      coordinates: {
+        latitude: "76.7214312",
+        longitude: "32.0320932",
+      },
+    },
+  ],
+  places_summary: {
+    category_count: [
+      {
+        category_id: 7,
+        category_name: "Stay",
+        places_count: 2,
+      },
+      {
+        category_id: 1,
+        category_name: "Food",
+        places_count: 2,
+      },
+    ],
+  },
+};
+
+console.log(temp.media);
+  let obj = {};
+// temp.media.map((value) => {
+//   value.place.place_id;
+// });
+
+// temp.media.map((value) => {
+//   console.log(value.place.place_id);
+//   obj[value.place.place_id] = value;
+// });
+
+// console.log(obj);
+
+temp.media.forEach((value) => {
+  if (obj.hasOwnProperty(value.place.place_id)) {
+    obj[value.place.place_id].push(value);
+  } else {
+    obj[value.place.place_id] = [];
+    obj[value.place.place_id].push(value);
+  }
+});
+
+console.log(obj);
